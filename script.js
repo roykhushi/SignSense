cabdc5c6234dc7c1239ea1513a0fd00787c9120e
Khushi Roy <roykhushi432@gmai.com>
2024-03-26 02:20:56 +0530

    changes

--- script.js
+++ script.js
@@ -2,20 +2,14 @@
 var typed = new Typed('#animate', {
     strings: ['SignSense!'], //content to show in the animation
     typeSpeed: 120, //typing speed in ms
-    backSpeed: 120, //backspeed in ms
+    backSpeed: 200, //backspeed in ms
     loop:true, // infinite loop of the animation
   });
 
 //gsap
 const tl = gsap.timeline();
-// tl.from("#navig li , #main1 h1, #main3 p , #navig img , #navig button," , {
-//     y: -100,
-//     duration: 1,
-//     delay :0.5,
-//     opacity: 0,
-//     stagger: 0.2
-// });
-tl.from("#navig li  , #main1 h1 , #main2 h1 , #main3 p , #navig img , #navig button" , {
+
+tl.from("#nav li, #nav img , #nav button , #main2 h1 , #main3 p" , {
     y: -100,
     duration: 1,
     delay :0.5,
@@ -23,13 +17,6 @@ tl.from("#navig li  , #main1 h1 , #main2 h1 , #main3 p , #navig img , #navig but
     stagger: 0.2
 });
 
-tl.fromTo("#btn" , {
-
-  duration: 1,
-  delay :0.5,
-  opacity: 0,
-  stagger: 0.2
-
-});
-
-
+tl.from("#get-started",{
+  scrollTrigger: "#get-started"
+})
